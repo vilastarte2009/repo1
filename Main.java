a8d91d60ca95876225fda29cf7df7362720d3f74
vilastarte2009 <vilastarte2009@gmail.com>
2023-10-12 18:24:48 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		
+
+	}
+
+}
